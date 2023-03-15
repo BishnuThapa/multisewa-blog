38c22bfb04c8379e6102b84b3130314e445447df
Bishnu <thapabishnu20@gmail.com>
2023-03-15 13:09:42 +0545

    mobile manu added

--- blog/settings.py
+++ blog/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-35!1v*+an$+5%1#(4^#sw&2^n(18-(r+4wo&t^s@qsn@5wsnks
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
